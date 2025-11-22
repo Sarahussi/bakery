6fd393e4290e299885773b42f5db4bb0b9035fc7
Joe0mran <yousif.edu.omran@gmail.com>
2025-11-22 16:32:28 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,19 +1,16 @@
-
-function addToCart(button, name, price) {
-
-      const qtyElement = button.parentElement.querySelector('.count2');
-      const quantity = parseInt(qtyElement?.innerText) || 1; 
+function addToCart(name, price){
+      /* const qtyElement = button.parentElement.querySelector('.count2');
+      const quantity = parseInt(qtyElement?.innerText) || 1; */
+      const quantity = 1;
       let cart = JSON.parse(localStorage.getItem('cart')) || [];
       const index = cart.findIndex(item => item.name === name);
 
       if (index !== -1) {
-        cart[index].quantity += quantity;
+        cart[index].quantity++;
       }else {
-        cart.push({name, price, quantity });
+        cart.push({name, price, quantity});
       }
 
       localStorage.setItem('cart', JSON.stringify(cart));
-      alert(name + ' x' + quantity + ' added to cart for ' + (price * quantity)+ ' EGP each');
-      qtyElement.innerText = 1;
+      alert(name + ' added to cart for ' + price + ' EGP each');
   }
-
